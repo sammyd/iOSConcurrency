4755ca3b6094b1d510949c5df4067337f90d3174
Sam Davies <sam@razeware.com>
2016-05-25 09:01:04 +0100

    Added descriptions around NSOperationQueue

--- ConcurrencyIniOS.playground/Pages/NSOperationQueue.xcplaygroundpage/Contents.swift
+++ ConcurrencyIniOS.playground/Pages/NSOperationQueue.xcplaygroundpage/Contents.swift
@@ -1,16 +1,18 @@
 /*:
- [NSOperation](@previous)
+ [⬅ NSOperation](@previous)
 
  ## NSOperationQueue
  
  There's not a lot of point of carefully wrapping up complex functionality in `NSOperation` object if you then have to call `start()` on each of them to actually begin execution.
  
+ Enter `NSOperationQueue`, which manages the execution of one or more `NSOperation` objects. Rather than having to handle threads directly, you instead pass your operations to a queue to be executed at the system's discretion. A queue can be configured to allow concurrent execution of the operations in the queue.
+ 
  */
-
-
 
 import UIKit
 
+
+//: Using the same tilt-shift operation, this time you've got a set of images rather than just one:
 let imageNames = ["dark_road_small", "train_day", "train_dusk", "train_night"]
 let images = imageNames.flatMap { UIImage(named: "\($0).jpg") }
 images
@@ -24,11 +26,21 @@ class TiltShiftOperation: NSOperation {
   }
 }
 
+
+//: Creating a queue is simple - using the default constructor:
 let queue = NSOperationQueue()
-queue.maxConcurrentOperationCount = 2
 
 var operations = [TiltShiftOperation]()
 
+/*:
+ Use the `addOperation()` method on `NSOperationQueue` to add each operation to the queue.
+ 
+ - important:
+ Adding operations to a queue is really "cheap"; although the operations can start executing as soon as they arrive on the queue, adding them is completely asynchronous.
+ \
+ You can see that here, with the result of the `duration` function:
+ 
+ */
 duration {
   for image in images {
     let op = TiltShiftOperation()
@@ -39,13 +51,22 @@ duration {
   }
 }
 
+/*:
+ * experiment:
+ You can control the maximum number of operations that a queue can execute simultaneously with the `maxConcurrentOperationCount` property. Setting this to `1` makes the queue a *serial* queue.
+ \
+ \
+ Try changing the value of this property below to see how it affects the time it takes for the queue to finish processing all operations
+ */
+queue.maxConcurrentOperationCount = 2
+
 duration {
   queue.waitUntilAllOperationsAreFinished()
 }
-  
+
+
+//: Check that all operations have filtered the image as expected
 let output = operations.flatMap { $0.outputImage }
 output
 
-
-
-//: [NSOperation Async](@next)
+//: [➡ NSOperation Async](@next)
