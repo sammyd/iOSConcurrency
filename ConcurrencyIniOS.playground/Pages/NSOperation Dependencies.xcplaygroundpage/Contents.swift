e0333ef3201951bc2d73fd796a9b2215f0831338
Sam Davies <sam@razeware.com>
2016-05-25 10:02:51 +0100

    Finished writing about NSOperation in practice

--- ConcurrencyIniOS.playground/Pages/NSOperation Dependencies.xcplaygroundpage/Contents.swift
+++ ConcurrencyIniOS.playground/Pages/NSOperation Dependencies.xcplaygroundpage/Contents.swift
@@ -1,8 +1,32 @@
 //: [Previous](@previous)
-
+/*:
+ ## Chaining NSOperations
+ 
+ Standalone operations are useful, but the true power from `NSOperation` comes from when you chain them together, building up a complex workflow from small parts.
+ 
+ In order to achieve this, you need to be able to pass the result of one operation to the next in the chain. This can be achieved using the `dependencies` property of `NSOperation`.
+ 
+ In this demo, you have two operations - one the same filtering one you've been using, and the other one that simulates loading the source image over a network.
+ 
+ - example:
+ You can imagine that you might need to build up a chain that involves even more steps:
+   - Retrieve source name from a data store
+   - Download an image from a network
+   - Decompress the image using a custom decompressor
+   - Apply a filter to the image
+   - Apply a second filter to the image
+   - Display the image
+ \
+ Each of these tasks could be modelled as an `NSOperation`, and the dependencies architecture would ensure that each operation will only begin once the appropriate data has been produced from a previous operation.
+ 
+ This approach allows you to break down complex tasks into smaller, reusable operations which compose together nicely. It can lead to cleaner code. However, be warned that as with any asynchronous code, debugging can become more challenging.
+ 
+ - note:
+ More than one operation can depend on another, so you can actually build a dependency graph - you're not limited to a simple chain. The great part of this is that you don't have to manage the scheduling of any of these - the operation queue handles it all for you.
+ */
 import UIKit
 
-
+//: An operation that loads the file "over the network":
 class ImageLoadOperation: AsyncOperation {
   var inputName: String?
   var outputImage: UIImage?
@@ -15,7 +39,7 @@ class ImageLoadOperation: AsyncOperation {
   }
 }
 
-
+//: The same filtering operation you saw before. The `main()` method now attempts to find an input image in its dependencies if the `inputImage` property hasn't already been set.
 class TiltShiftOperation: NSOperation {
   var inputImage: UIImage?
   var outputImage: UIImage?
@@ -33,7 +57,7 @@ class TiltShiftOperation: NSOperation {
 }
 
 
-
+//: Rather than coding directly to concrete implementations, define a protocol that represents _"an object that can provide data to an image filter"_. This makes the code that searches dependencies far less brittle.
 protocol FilterDataProvider {
   var outputImage: UIImage? { get }
 }
@@ -43,6 +67,12 @@ extension ImageLoadOperation: FilterDataProvider {
 }
 
 
+/*:
+ To add a dependency to an `NSOperation` object, use the `addDependency()` method.
+ However, it's a rare case when a custom operator can make the code easier to read.
+ - important:
+ Heed all the usual warnings about custom operators. This is a situation where they can offer genuine clarity, but that isn't often the case.
+ */
 infix operator |> { associativity left precedence 150 }
 func |>(lhs: NSOperation, rhs: NSOperation) -> NSOperation {
   rhs.addDependency(lhs)
@@ -50,16 +80,17 @@ func |>(lhs: NSOperation, rhs: NSOperation) -> NSOperation {
 }
 
 
+//: Create the relevant operations
 let imageLoad = ImageLoadOperation()
-imageLoad.inputName = "train_day.jpg"
-
 let filter = TiltShiftOperation()
 
+//: Set the input parameter on the image loading operation:
+imageLoad.inputName = "train_day.jpg"
 
+//: And set the dependency chain
 imageLoad |> filter
 
-
-
+//: Add both operations to the operation queue
 let queue = NSOperationQueue()
 duration {
   queue.addOperations([imageLoad, filter], waitUntilFinished: true)
@@ -69,4 +100,4 @@ duration {
 filter.outputImage
 
 
-//: [Next](@next)
+//: [➡ NSOperation in Practice](@next)
--- ConcurrencyIniOS.playground/Pages/NSOperation in Practice.xcplaygroundpage/Contents.swift
+++ ConcurrencyIniOS.playground/Pages/NSOperation in Practice.xcplaygroundpage/Contents.swift
@@ -1,8 +1,18 @@
-//: [Previous](@previous)
-
+//: [⬅ Chaining NSOperations](@previous)
+/*:
+ ## NSOperations in Practice
+ 
+ You've seen how powerful `NSOperation` is, but not really seen it fix a real-world problem.
+ 
+ This playground page demonstrates how you can use `NSOperation` to load and filter images for display in a table view, whilst maintaining the smooth scroll effect you expect from table views.
+ 
+ This is a common problem, and comes from the fact that if you attempt expensive operations synchronously, you'll block the main queue (thread). Since this is used for rendering the UI, you cause your app to become unresponsive - temporarily freezing.
+ 
+ The solution is to move data loading off into the background, which can be achieved easily with `NSOperation`.
+ 
+ */
 import UIKit
 import XCPlayground
-
 
 let tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 320, height: 720))
 tableView.registerClass(ImageCell.self, forCellReuseIdentifier: "ImageCell")
@@ -10,7 +20,7 @@ XCPlaygroundPage.currentPage.liveView = tableView
 tableView.rowHeight = 250
 
 
-
+//: `ImageProvider` is a class that is responsible for loading and processing an image. It creates the relevant operations, chains them together, pops them on a queue and then ensures that the output is passed back appropriately
 class ImageProvider {
   
   let queue = NSOperationQueue()
@@ -33,11 +43,13 @@ class ImageProvider {
   }
 }
 
+//: `DataSource` is a class that represents the table's datasource and delegate
 class DataSource: NSObject {
   var imageNames = [String]()
   var imageProviders = [NSIndexPath : ImageProvider]()
 }
 
+//: Possibly the simplest implementation of `UITableViewDataSource`:
 extension DataSource: UITableViewDataSource {
   func numberOfSectionsInTableView(tableView: UITableView) -> Int {
     return 1
@@ -73,14 +85,16 @@ extension DataSource: UITableViewDelegate {
   }
 }
 
-
+//: Create a datasource and provide a list of images to display
 let ds = DataSource()
 ds.imageNames = ["dark_road_small.jpg", "train_day.jpg", "train_dusk.jpg", "train_night.jpg", "dark_road_small.jpg", "train_day.jpg", "train_dusk.jpg", "train_night.jpg", "dark_road_small.jpg", "train_day.jpg", "train_dusk.jpg", "train_night.jpg", "dark_road_small.jpg", "train_day.jpg", "train_dusk.jpg", "train_night.jpg"]
 
 tableView.dataSource = ds
 tableView.delegate = ds
 
-
-
-
-//: [Next](@next)
+/*:
+ - note:
+ This implementation for a table view is not complete, but instead meant to demonstrate how you can use `NSOperation` to improve the scrolling performance.
+ 
+ [➡ Grand Central Dispatch](@next)
+ */
